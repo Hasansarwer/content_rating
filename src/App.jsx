f22ab95974c797d44759c27e4c12e97072a9aa1e
Hasansarwer <hasanmathju@gmail.com>
2024-12-12 20:46:21 +0600

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [likes, setLikes] = useState(0)
+  const [dislikes, setDislikes] = useState(0)
+  const [totalRating, setTotalRating] = useState(0)
+
+  const handleLike = () => {
+    setLikes(likes + 1)
+    setTotalRating(totalRating + 1)
+  }
+
+  const handleDislike = () => {
+    setDislikes(dislikes + 1)
+    setTotalRating(totalRating + 1)
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="content-rating">
+        <p>TODO</p>
+        <div className="rating-button">
+          <button className="like-button"
+          onClick={() => handleLike()}
+          >Like ({likes})</button>
+          <button className="dislike-button"
+          onClick={() => handleDislike()}
+          >Dislike ({dislikes})</button>
+        </div>
+        <p>Total rating: {totalRating}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
+      
+      
     </>
   )
 }
